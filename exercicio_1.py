228e9b1b61302e2d1141ebabb6d8cbba6e432848
Quickich <163673540+Quickich@users.noreply.github.com>
2024-04-02 07:16:16 -0300

    Update exercicio_1allan.py
    Allan Kipper

--- exercicio_1.py
+++ exercicio_1.py
@@ -17,3 +17,12 @@ Digite um número:
 
 Dica: Lembre do comando de resto da divisão inteira!
 """
+
+
+numero = float(input("Digite um número:"))
+
+if numero/2 = 0:
+  print("O número é par")
+else:
+  print("O número é ímpar")
+  
