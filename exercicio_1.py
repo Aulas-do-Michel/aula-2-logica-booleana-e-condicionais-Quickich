dd3df99892576eab0459cede6c041248b2f5c1c1
Quickich <163673540+Quickich@users.noreply.github.com>
2024-04-02 07:33:31 -0300

    Update exercicio_1.py
    allan km 1

--- exercicio_1.py
+++ exercicio_1.py
@@ -21,7 +21,7 @@ Dica: Lembre do comando de resto da divisão inteira!
 
 numero = float(input("Digite um número:"))
 
-if numero / 2 % 0:
+if numero % 2 = 0:
   print("O número é par")
 else:
   print("O número é ímpar")
